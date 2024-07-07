31ec03cb0093fa7113e9235ed5da7420f15186b9
abel-otegbola <abel15655@gmail.com>
2024-07-08 00:22:04 +0100

    feat: create cart functionality using context

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/home'
 import Topbar from './components/topbar/topbar'
 import Footer from './components/footer/footer'
+import CartContextProvider from './context/cartContext'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Topbar />
-      <Routes>
-        <Route path={"/"} exact element={<Home />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <Topbar />
+        <Routes>
+          <Route path={"/"} exact element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </CartContextProvider>
   )
 }
 
