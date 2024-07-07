163d8bfa1ce1d0a768691fb8890675e61568e8ca
abel-otegbola <abel15655@gmail.com>
2024-07-07 21:04:35 +0100

    feat: add social icons to assets

---
+++ src/assets/icons/twitterIcon.jsx
@@ -0,0 +1,19 @@
+export default function TwitterIcon({className}) {
+    return (
+        <svg width="24" height="25" className={className} viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_15_173)">
+                <mask id="mask0_15_173" style={{maskType: "luminance"}} maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="25">
+                    <path d="M0 0.5H24V24.5H0V0.5Z" fill="white"/>
+                </mask>
+                <g mask="url(#mask0_15_173)">
+                    <path d="M18.9 1.62451H22.5806L14.5406 10.8371L24 23.3754H16.5943L10.7897 15.7725L4.15543 23.3754H0.471429L9.07029 13.5182L0 1.62623H7.59429L12.8331 8.57423L18.9 1.62451ZM17.6057 21.1674H19.6457L6.48 3.71765H4.29257L17.6057 21.1674Z" fill="#40E0D0"/>
+                </g>
+            </g>
+            <defs>
+                <clipPath id="clip0_15_173">
+                    <rect width="24" height="24" fill="white" transform="translate(0 0.5)"/>
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
