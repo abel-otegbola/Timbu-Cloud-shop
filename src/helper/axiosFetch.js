109f213be375ae6ae47d87326037f98ebb673a0b
abel-otegbola <abel15655@gmail.com>
2024-07-13 10:15:40 +0100

    feat: add source rewrites for /api in vercel.config

--- src/helper/axiosFetch.js
+++ src/helper/axiosFetch.js
@@ -5,8 +5,6 @@ export const getProducts = async () => {
     const Appid = import.meta.env.VITE_APP_ID
     const Apikey = import.meta.env.VITE_API_KEY;
 
-    const URL = import.meta.env.DEV ? "/api" : "https://api.timbu.cloud"
-
-    return await axios.get(`${URL}/products?organization_id=${ORG_ID}&reverse_sort=false&page=1&size=10&Appid=${Appid}&Apikey=${Apikey}`
+    return await axios.get(`/api/products?organization_id=${ORG_ID}&reverse_sort=false&page=1&size=10&Appid=${Appid}&Apikey=${Apikey}`
     )
 }
